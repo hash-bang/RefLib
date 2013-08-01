fff4f8b48df30ef865f7971e1da9da5ec010ab76
Matt Carter <m@ttcarter.com>
2013-08-01 11:26:49 +1000

    Added SetXML() function to read in EndNote XML

--- endnote.php
+++ endnote.php
@@ -23,21 +23,22 @@ class PHPEndNote {
 	*	* year - String (optional) - FIXME: Explain format
 	*	* date - String (optional) - FIXME: Explain format
 	*	* abstract - String (optional)
-	*	* url - String (optional)
+	*	* urls - Array
 	*	* notes - String (optional)
+	*	* isbn - String (optional)
 	*
 	* @var array
 	*/
-	var $references;
+	var $refs;
 
 	/**
-	* Return the raw XML of the $references array
-	* @see $references
+	* Return the raw XML of the $refs array
+	* @see $refs
 	*/
 	function GetXML() {
 		$out = '<?xml version="1.0" encoding="UTF-8"?><xml><records>';
 		$number = 0;
-		foreach ($this->references as $id => $ref) {
+		foreach ($this->refs as $id => $ref) {
 			$out .= '<record>';
 			$out .= '<database name="CREBP-SearchTool.enl" path="C:\CREBP-SearchTool.enl">CREBP-SearchTool.enl</database>';
 			$out .= '<source-app name="EndNote" version="16.0">EndNote</source-app>';
@@ -96,5 +97,56 @@ class PHPEndNote {
 		header('Content-type: text/plain');
 		header('Content-Disposition: attachment; filename="' . $filename . '"');
 		echo $this->GetXML();
+	}
+
+	function SetXML($xml) {
+		$dom = new SimpleXMLElement($xml);
+		foreach ($dom->records->record as $record) {
+			$ref = array(
+				'authors' => array(),
+				'urls' => array(),
+				'title' => '',
+			);
+			foreach ($record->xpath('contributors/authors/author/style/text()') as $authors) 
+				$ref['authors'][] = end($authors);
+
+			foreach ($record->xpath('urls/related-urls/url/style/text()') as $url) 
+				$ref['urls'][] = end($url);
+
+			if ($find = $record->xpath("titles/title/style/text()"))
+				$ref['title'] = end(current($find));
+			if ($find = $record->xpath("titles/secondary-title/style/text()"))
+				$ref['title-secondary'] = end(current($find));
+			if ($find = $record->xpath("titles/short-title/style/text()"))
+				$ref['title-short'] = end(current($find));
+			if ($find = $record->xpath("periodical/full-title/style/text()"))
+				$ref['periodical-title'] = end(current($find));
+			if ($find = $record->xpath("dates/year/style/text()"))
+				$ref['year'] = end(current($find));
+			if ($find = $record->xpath("dates/pub-dates/date/style/text()"))
+				$ref['date'] = end(current($find));
+
+			// Simple key=>vals
+			foreach (array(
+				'auth-address' => 'address',
+				'pages' => 'pages',
+				'volume' => 'volume',
+				'number' => 'number',
+				'section' => 'section',
+				'abstract' => 'abstract',
+				'isbn' => 'isbn',
+				'notes' => 'notes',
+				'research-notes' => 'notes',
+			) as $enkey => $ourkey) {
+				if (! $find = $record->xpath("$enkey/style/text()") )
+					continue;
+				$ref[$ourkey] = end(current($find));
+			}
+			$this->refs[] = $ref;
+		}
+	}
+
+	function SetXMLFile($filename) {
+		$this->SetXML(file_get_contents($filename));
 	}
 }
