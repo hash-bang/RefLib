054297bb865062425acec9cf7d122d333d348ae0
Matt Carter <m@ttcarter.com>
2013-11-12 15:21:12 +1000

    Added support for custom{1..7} fields

--- endnote.php
+++ endnote.php
@@ -28,6 +28,7 @@ class PHPEndNote {
 	*	* isbn - String (optional)
 	*	* label - String (optional)
 	*	* caption - String (optional)
+	*	* custom{1..7} - String (optional)
 	*
 	* @var array
 	*/
@@ -135,6 +136,13 @@ class PHPEndNote {
 				'isbn' => 'isbn',
 				'label' => 'label',
 				'caption' => 'caption',
+				'custom1' => 'custom1',
+				'custom2' => 'custom2',
+				'custom3' => 'custom3',
+				'custom4' => 'custom4',
+				'custom5' => 'custom5',
+				'custom6' => 'custom6',
+				'custom7' => 'custom7',
 			) as $enkey => $ourkey)
 				$out .= "<$enkey><style face=\"normal\" font=\"default\" size=\"100%\">" . (isset($ref[$ourkey]) && $ref[$ourkey] ? $this->_export($ref[$ourkey]) : '') . "</style></$enkey>";
 
@@ -210,6 +218,13 @@ class PHPEndNote {
 				'research-notes' => 'notes',
 				'label' => 'label',
 				'caption' => 'caption',
+				'custom1' => 'custom1',
+				'custom2' => 'custom2',
+				'custom3' => 'custom3',
+				'custom4' => 'custom4',
+				'custom5' => 'custom5',
+				'custom6' => 'custom6',
+				'custom7' => 'custom7',
 			) as $enkey => $ourkey) {
 				if (! $find = $record->xpath("$enkey/style/text()") )
 					continue;
